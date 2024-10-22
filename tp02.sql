fe9349b511110bf668d405ae8e6cd0ef84090cbd
Mohamed Bachi <mohamed.bachi@epitech.eu>
2024-10-22 10:10:51 +0200

    insertionsCorrigé

--- tp02.sql
+++ tp02.sql
@@ -1,14 +1,32 @@
-INSERT INTO fournisseur (ID, NOM) VALUES (1, 'Française d’Imports');
-INSERT INTO fournisseur (ID, NOM) VALUES (2, 'FDM SA');
-INSERT INTO fournisseur (ID, NOM) VALUES (3, 'Dubois & Fils');
+-- Sélection de la base de données
+USE COMPTA2;
 
-INSERT INTO article (ID, REF, DESIGNATION, PRIX, ID_FOU) VALUES (1, 'A01', 'Perceuse P1', 74.99, 1);
-INSERT INTO article (ID, REF, DESIGNATION, PRIX, ID_FOU) VALUES (2, 'F01', 'Boulon laiton 4 x 40 mm (sachet de 10)', 2.25, 2);
-INSERT INTO article (ID, REF, DESIGNATION, PRIX, ID_FOU) VALUES (3, 'F02', 'Boulon laiton 5 x 40 mm (sachet de 10)', 4.45, 2);
-INSERT INTO article (ID, REF, DESIGNATION, PRIX, ID_FOU) VALUES (4, 'D01', 'Boulon laiton 5 x 40 mm (sachet de 10)', 4.40, 3);
-INSERT INTO article (ID, REF, DESIGNATION, PRIX, ID_FOU) VALUES (5, 'A02', 'Meuleuse 125mm', 37.85, 1);
-INSERT INTO article (ID, REF, DESIGNATION, PRIX, ID_FOU) VALUES (6, 'D03', 'Boulon acier zingué 4 x 40 mm (sachet de 10)', 1.80, 3);
-INSERT INTO article (ID, REF, DESIGNATION, PRIX, ID_FOU) VALUES (7, 'A03', 'Perceuse à colonne', 185.25, 1);
-INSERT INTO article (ID, REF, DESIGNATION, PRIX, ID_FOU) VALUES (8, 'D04', 'Coffret mèches à bois', 12.25, 3);
-INSERT INTO article (ID, REF, DESIGNATION, PRIX, ID_FOU) VALUES (9, 'F03', 'Coffret mèches plates', 6.25, 2);
-INSERT INTO article (ID, REF, DESIGNATION, PRIX, ID_FOU) VALUES (10, 'F04', 'Fraises d’encastrement', 8.14, 2);
+-- Insertion des fournisseurs
+INSERT INTO FOURNISSEUR (ID, NOM) 
+VALUES 
+(1, 'Française d''Imports'),
+(2, 'FDM SA'),
+(3, 'Dubois & Fils');
+
+-- Insertion des articles
+INSERT INTO ARTICLE (ID, REF, DESIGNATION, PRIX, ID_FOU) 
+VALUES 
+(1, 'A01', 'Perceuse P1', 74.99, 1),
+(2, 'F01', 'Boulon laiton 4 x 40 mm (sachet de 10)', 2.25, 2),
+(3, 'F02', 'Boulon laiton 5 x 40 mm (sachet de 10)', 4.45, 2),
+(4, 'D01', 'Boulon laiton 5 x 40 mm (sachet de 10)', 4.40, 3),
+(5, 'A02', 'Meuleuse 125mm', 37.85, 1),
+(6, 'D03', 'Boulon acier zingué 4 x 40mm (sachet de 10)', 1.80, 3),
+(7, 'A03', 'Perceuse à colonne', 185.25, 1),
+(8, 'D04', 'Coffret mêches à bois', 12.25, 3),
+(9, 'F03', 'Coffret mêches plates', 6.25, 2),
+(10, 'F04', 'Fraises d''encastrement', 8.14, 2);
+
+-- Insertion du bon de commande auprès du fournisseur "Française d'Imports"
+INSERT INTO BON (ID, NUMERO, DELAI, ID_FOU) VALUES (1, 1, 3, 1);
+
+-- Insertion de la composition du bon de commande n°001
+INSERT INTO COMPO (ID_ART, ID_BON, QTE) VALUES
+(1, 1, 3),  -- 3 Perceuses P1
+(5, 1, 4),  -- 4 Meuleuses 125mm
+(7, 1, 1);  -- 1 Perceuse à colonne
